20961fe0dafe047a82991d6fb4d203a2bafb211b
Kaminyou <ikaminyou@gmail.com>
2021-10-04 12:40:54 +0800

    answer to q1

---
+++ python-ability-test/01_Ice-Cream-Machine/answer.py
@@ -0,0 +1,16 @@
+class IceCreamMachine:
+    
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+        
+    def scoops(self):
+        out = []
+        for i in self.ingredients:
+            for j in self.toppings:
+                out.append([i, j])
+        return out
+
+if __name__ == "__main__":
+    machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+    print(machine.scoops()) #should print[['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
