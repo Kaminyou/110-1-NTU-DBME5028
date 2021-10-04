06ed85798661b74578c7277cb47eed126ee7a11d
Kaminyou <ikaminyou@gmail.com>
2021-10-04 14:48:04 +0800

    answer to q6

---
+++ python-ability-test/06_Route-Planner/answer.py
@@ -0,0 +1,31 @@
+def route_exists(from_row, from_column, to_row, to_column, map_matrix):
+    def __valid(row, column, max_row, max_column):
+        if 0 <= row and row < max_row and 0 <= column and column < max_column and map_matrix[row][column]:
+            return True
+        return False
+
+    max_row = len(map_matrix)
+    max_column = len(map_matrix[0])
+    
+    queue = [(from_row, from_column)]
+    visited = {(from_row, from_column)}
+    
+    ds = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    while queue:
+        row, column = queue.pop(0)
+        if row == to_row and column == to_column:
+            return True
+        for d in ds:
+            if __valid(row+d[0], column+d[1], max_row, max_column) and (row+d[0], column+d[1]) not in visited:
+                queue.append((row+d[0], column+d[1]))
+                visited.add((row+d[0], column+d[1]))
+    return False
+
+if __name__ == '__main__':
+    map_matrix = [
+        [True, False, False],
+        [True, True, False],
+        [False, True, True]
+    ]
+
+    print(route_exists(0, 0, 2, 2, map_matrix))
